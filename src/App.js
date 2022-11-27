b27eb0081c709996e57c3e92c0040292c150a491
ZelenovaV <107313873+ZelenovaV@users.noreply.github.com>
2022-11-27 15:02:27 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <header className="App-header"> Dictionary</header>
     </div>
   );
 }
